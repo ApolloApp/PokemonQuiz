3d547cfffafaa9e45fa330e7ad8c8993f1b79191
emitpool <canalp3rry@gmail.com>
2022-05-07 16:09:41 -0300

    Make QuizActivity

---
+++ app/src/main/java/com/candoanything/myquizapp/Constants.kt
@@ -0,0 +1,70 @@
+package com.candoanything.myquizapp
+
+object Constants {
+
+    fun getQuestion():ArrayList<Question>{
+        val questionList = ArrayList<Question>()
+
+        val que1 = Question(
+            1, "What has to be broken before you can use it?",
+            "An light",
+            "An wall",
+            "An egg",
+            "An hope",
+            3
+
+        )
+        questionList.add(que1)
+
+        // 2
+        val que2 = Question(
+            2, "A cowboy rode into town on Friday afternoon. He spent three days in town and then rode out on Friday. How is that possible?",
+            "Friday is the name of the man's horse.",
+            "Friday is enemy of the man's.",
+            "Friday is the name of the man father",
+            "Friday is nothing",
+            1
+
+        )
+        questionList.add(que2)
+
+        // 3
+        val que3 = Question(
+            3, " I am pronounced as one letter, but I am written with three. Me and my twin are either blue, green, or brown. What am I?",
+            "An light",
+            "An eye.",
+            "An man",
+            "An lucky",
+            2
+
+        )
+        questionList.add(que3)
+
+        // 4
+        val que4 = Question(
+            4, "What is full of holes but can still hold water?",
+            "An dark",
+            "An wall",
+            "An egg",
+            "An sponge",
+            4
+
+        )
+        questionList.add(que4)
+
+        // 5
+        val que5 = Question(
+            5, "What belongs to you but is used more by others?",
+            "your thought",
+            "your name",
+            "your strength",
+            "your style",
+            3
+
+        )
+        questionList.add(que5)
+
+        return questionList
+    }
+
+}
